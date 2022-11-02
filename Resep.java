80625f665c014ed9fe130f119f28baaf648e8823
luckydxd <luckydawamuzdzikri@gmail.com>
2022-11-02 16:22:56 +0700

    inisiasi resep cirebonpride

---
+++ Resep.java
@@ -0,0 +1,8 @@
+public class Resep{
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        System.out.println("1.Bawang");
+        System.out.println("2.Ketimun");
+        
+    }
+}
